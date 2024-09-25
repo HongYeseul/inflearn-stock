a588573959097e1011071cba40e24316d88225fc
HongYeseul <ghd8119@naver.com>
2024-09-25 16:58:08 +0900

    feat: 동시성 방법 1 synchronized 사용

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -14,8 +14,8 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    @Transactional
-    public void decreaseQuantity(Long id, Long quantity) {
+//    @Transactional
+    public synchronized void decreaseQuantity(Long id, Long quantity) {
         // Stock 조회
         // 재고 감소
         // 갱신된 값을 저장
