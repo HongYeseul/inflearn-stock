45384a93d22a6c7b2b6a09751e87857dc0e66077
HongYeseul <ghd8119@naver.com>
2024-09-25 20:37:28 +0900

    feat: 2. OptimisticLock 방법으로 구현하기

---
+++ src/test/java/com/example/stock/facade/OptimisticLockStockFacadeTest.java
@@ -0,0 +1,58 @@
+package com.example.stock.facade;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class OptimisticLockStockFacadeTest {
+
+    @Autowired
+    private OptimisticLockStockFacade optimisticLockStockFacade;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void before() {
+        stockRepository.saveAndFlush(new Stock(1L, 100L));
+    }
+
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        // latch는 다른 스레드의 작업이 끝날 때까지 대기 할 수 있도록 도와주는 클래스
+
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    optimisticLockStockFacade.decreaseQuantity(1L, 1L);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100 - (1*100) = 0
+        assertEquals(0L, stock.getQuantity());
+    }
+}
